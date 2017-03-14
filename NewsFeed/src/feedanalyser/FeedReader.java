2f79f8f8bfb2034e172ca1d9ae84b563b8cb1c77
Florian Schmidt <florian.schmidt1995@gmx.de>
2017-03-14 16:58:11 +0100

    simplified code of Feedreader.java added a comment with example URLs which behave very different

--- NewsFeed/src/feedanalyser/FeedReader.java
+++ NewsFeed/src/feedanalyser/FeedReader.java
@@ -1,10 +1,13 @@
 package feedanalyser;
 
 import java.net.URL;
-import java.sql.Date;
+//import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 
+import com.sun.syndication.feed.module.DCModule;
 import com.sun.syndication.feed.module.DCModuleImpl;
+import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndEntryImpl;
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.io.SyndFeedInput;
@@ -19,37 +22,31 @@ import com.sun.syndication.io.XmlReader;
 public class FeedReader {
 
     public static void main(String[] args) {
-        boolean ok = false;
-        if (true) {
-            try {
-                URL feedUrl = new URL("http://www.tagesschau.de/xml/rss2");
+        try {
+        	// !!! Different behavior for different feeds!
+        	// http://www.tagesschau.de/xml/rss2
+        	// http://www.tagesschau.de/newsticker.rdf
+            URL feedUrl = new URL("http://www.tagesschau.de/xml/atom/");
 
-                SyndFeedInput input = new SyndFeedInput();
-                SyndFeed feed = input.build(new XmlReader(feedUrl));
-                
-    
-                List<SyndEntryImpl> entries = feed.getEntries();
-                SyndEntryImpl entry = entries.get(0);
-                
-                List<DCModuleImpl> modules = entry.getModules();
-                DCModuleImpl module = modules.get(0);
-                Date date = (Date) module.getDate();
-                
-                System.out.println(feed);
+            SyndFeedInput input = new SyndFeedInput();
+            SyndFeed feed = input.build(new XmlReader(feedUrl));
+            
 
-                ok = true;
-            }
-            catch (Exception ex) {
-                ex.printStackTrace();
-                System.out.println("ERROR: "+ex.getMessage());
-            }
+            List<SyndEntry> entries = feed.getEntries();
+            SyndEntry entry = entries.get(0);
+            
+            List<DCModule> modules = entry.getModules();
+            DCModule module = modules.get(0);
+            Date date = (Date) module.getDate();           
+            System.out.println("Date from modules: " + date);
+            
+            System.out.println("Date from entry directly: " + entry.getUpdatedDate());
+            
+            System.out.println(feed);
         }
-
-        if (!ok) {
-            System.out.println();
-            System.out.println("FeedReader reads and prints any RSS/Atom feed type.");
-            System.out.println("The first parameter must be the URL of the feed to read.");
-            System.out.println();
+        catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("ERROR: "+ex.getMessage());
         }
     }
 
