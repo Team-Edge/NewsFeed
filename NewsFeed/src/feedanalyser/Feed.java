eb631fcdc2033079ba6e124f97ce57418a7053ee
z003hkey <z003hkey@MD1HMRPC.ww004.siemens.net>
2017-03-16 15:08:29 +0100

    feed

--- NewsFeed/src/feedanalyser/Feed.java
+++ NewsFeed/src/feedanalyser/Feed.java
@@ -1,13 +1,22 @@
 package feedanalyser;
 
-import java.sql.Date;
+import java.util.Date;
 
 public class Feed {
 
 	private String title;
 	private String description;
-	private String imageURL;
 	private Date pubDate;
 	private String text;
 	private String URL;
+	
+	public Feed(String title, String description, Date pubDate, String text, String URL){
+		
+		this.title = title;
+		this.description = description;
+		if(pubDate != null) this.pubDate = pubDate;
+		else this.pubDate = new java.util.Date();
+		this.text = text;
+		this.URL = URL;
+	}
 }
