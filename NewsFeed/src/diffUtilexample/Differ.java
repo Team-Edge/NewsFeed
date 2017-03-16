4ef9191f70563c0b4c04831619e557dbb51c257d
z003hkey <z003hkey@MD1HMRPC.ww004.siemens.net>
2017-03-16 12:50:40 +0100

    Differ finished

--- NewsFeed/src/diffUtilexample/Differ.java
+++ NewsFeed/src/diffUtilexample/Differ.java
@@ -1,19 +1,55 @@
 package diffUtilexample;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import difflib.DiffUtils;
 import difflib.Patch;
 import difflib.*;
+import difflib.Delta.TYPE;
 
 public class Differ {
 
-	public Differ() {
-		// TODO Auto-generated constructor stub
+	private List<String> insert;
+	private List<String> delete;
+	private List<String> change;
+	
+	@SuppressWarnings("unchecked")
+	public Differ(List<String> file1, List<String> file2) {
+		
+		Patch patch = DiffUtils.diff(file1, file2);
+		
+		insert = new ArrayList<String>();
+		delete = new ArrayList<String>();
+		change = new ArrayList<String>();
+		
+		for(Delta delta : patch.getDeltas())
+		{
+			TYPE type = delta.getType();
+			if(type == TYPE.INSERT) insert.addAll((List<String>) delta.getRevised().getLines());
+			if(type == TYPE.DELETE) delete.addAll((List<String>) delta.getOriginal().getLines());
+			if(type == TYPE.CHANGE) change.addAll((List<String>) delta.getOriginal().getLines());
+		}
 	}
 
-	public static void main(String[] args) {
+	public List<String> getInsert(){
+		
+		return insert;
+	}
+	
+	public List<String> getDelete(){
+		
+		return delete;
+	}
+	
+	public List<String> getChange(){
+		
+		return change;
+	}
+}	
+	
+	/*public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List<String> file1 = new ArrayList<String>();
 		file1.add("3");
@@ -28,16 +64,15 @@ public class Differ {
 		
 		
 		Patch patch = DiffUtils.diff(file1, file2);
-		
-		System.out.println(patch);
+		List<String> insert = null;
+		List<String> delete = null;
 		for(Delta delta : patch.getDeltas())
 		{
-			System.out.println(delta);
-			List<String> revised = (List<String>) delta.getRevised().getLines();
-			List<String> original = (List<String>) delta.getOriginal().getLines();
+			TYPE type = delta.getType();
+			if(type == TYPE.INSERT) insert = (List<String>) delta.getRevised().getLines();
+			if(type == TYPE.DELETE) delete = (List<String>) delta.getOriginal().getLines();
 		}
-		System.out.println(patch);
 		
-	}
-
-}
+		System.out.println(insert);
+		System.out.println(delete);
+	}*/
