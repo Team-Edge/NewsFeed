709903893ffb50dd7df137980b5eb2cda0d93511
Severin Neuner <severin.neuner@outlook.com>
2017-05-03 10:56:34 +0200

    setup and cleanUp for test implemented

--- NewsFeed/src/tests/ArchiverTest.java
+++ NewsFeed/src/tests/ArchiverTest.java
@@ -1,21 +1,32 @@
 package tests;
 
 import static org.junit.Assert.*;
-import org.junit.Test;
+import org.junit.*;
 
 import archiver.*;
 import program.Configuration;
 
 public class ArchiverTest {
 
+	@Before
+	public void setup()
+	{
+		Configuration.loadFromFile("./TestFiles/testWrongLoginConfig.txt");
+	}
+	
 	@Test(expected=Exception.class)
 	public void testArchiver()
 	{
 		Archiver a = new Archiver();
-		Configuration.loadFromFile("./TestFiles/testWrongLoginConfig.txt");
 		assertTrue(a.needsDB());
 		assertEquals(a.toString(),"Archiver");
 		a.run();
+	}
+	
+	@After
+	public void cleanUp()
+	{
+		Configuration.loadFromFile("./config.txt");
 	}
 
 }
